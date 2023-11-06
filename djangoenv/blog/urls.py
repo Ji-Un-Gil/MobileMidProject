2ececb825cdd8dfdc0aaa1c5c55043c7fd2595dc
길지운 <wldns2577@khu.ac.kr>
2023-11-07 01:57:47 +0900

    fix: GET API 호출 시 발생하는 PK 네이밍 오류 처리

--- djangoenv/blog/urls.py
+++ djangoenv/blog/urls.py
@@ -9,7 +9,7 @@ router.register('posts', views.PostViewSet)
 urlpatterns = [
     path("", views.post_list, name="post_list"),
     path('api/', include(router.urls)),
-    path('api/posts/info/<int:post_id>/', views.PostDetail.as_view(), name='post_detail'),
+    path('api/posts/info/<int:id>/', views.PostDetail.as_view(), name='post_detail'),
     path('api/posts/new/', views.PostCreate.as_view(), name='post_create'),
-    path('api/posts/<int:post_id>/', views.delete_post, name='delete_post'),
+    path('api/posts/<int:id>/', views.delete_post, name='delete_post'),
 ]
