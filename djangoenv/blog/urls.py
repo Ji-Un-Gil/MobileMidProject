c3594e48e6dd2fbba7f94c550f16c8b09812084b
길지운 <wldns2577@khu.ac.kr>
2023-11-07 02:34:43 +0900

    fix: API 인식 오류 처리

--- djangoenv/blog/urls.py
+++ djangoenv/blog/urls.py
@@ -7,8 +7,8 @@ router.register(r'posts', views.PostViewSet)
 
 urlpatterns = [
     path("", views.post_list, name="post_list"),
-    path('api/posts/<int:id>/', views.delete_post, name='delete_post'),
+    path('api/', include(router.urls)),
     path('api/posts/info/<int:id>/', views.PostDetail.as_view(), name='post_detail'),
     path('api/posts/new/', views.PostCreate.as_view(), name='post_create'),
-    path('api/', include(router.urls)),
+    path('api/posts/<int:id>/', views.delete_post, name='delete_post'),
 ]
