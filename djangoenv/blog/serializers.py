c3f1e0462ece44aabf1ac8601b0591a145235a51
길지운 <wldns2577@khu.ac.kr>
2023-11-06 23:11:24 +0900

    refactor: 필드명 수정

--- djangoenv/blog/serializers.py
+++ djangoenv/blog/serializers.py
@@ -5,4 +5,4 @@ from rest_framework import serializers
 class PostSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Post
-        fields = ('title', 'text', 'created_date', 'published_date', 'image_url')
+        fields = ('title', 'text', 'created_date', 'published_date', 'image')
