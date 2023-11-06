c74404cdfec031d9c3c29d7e7c72554acaf89cf8
길지운 <wldns2577@khu.ac.kr>
2023-11-07 02:18:53 +0900

    fix: POST API 405 오류 처리

--- djangoenv/blog/urls.py
+++ djangoenv/blog/urls.py
@@ -1,14 +1,12 @@
 from django.urls import path
-from django.urls import include
 from rest_framework import routers
 from . import views
 
 router = routers.DefaultRouter()
-router.register('posts', views.PostViewSet)
 
 urlpatterns = [
     path("", views.post_list, name="post_list"),
-    path('api/', include(router.urls)),
+    path('api/posts/', views.PostViewSet, name='post_view_set'),
     path('api/posts/info/<int:id>/', views.PostDetail.as_view(), name='post_detail'),
     path('api/posts/new/', views.PostCreate.as_view(), name='post_create'),
     path('api/posts/<int:id>/', views.delete_post, name='delete_post'),
